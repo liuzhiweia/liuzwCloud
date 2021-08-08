50ac67e07bf55a18f263688c98fd6b7e7fb2899c
liuzhiwei <1298433393@qq.com>
2021-08-08 22:48:31 +0800

    添加配置文件已经启动累，修改配置文件内的数据流连接密码为加密模式

---
+++ cloud-provider-payment/src/main/java/com/liuzw/springcloud/PaymentServiceApplication.java
@@ -0,0 +1,18 @@
+package com.liuzw.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author: liuzw
+ * @date: 2021/8/8 7:48 下午
+ * @e-mail: liuzhiwei1@ylzinfo.com
+ * -------------------------------
+ * @description： springboot启动类
+ */
+@SpringBootApplication
+public class PaymentServiceApplication {
+    public static void main(String[] args){
+        SpringApplication.run(PaymentServiceApplication.class,args);
+    }
+}
